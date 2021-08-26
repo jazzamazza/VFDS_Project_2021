419b26ff9084ba08649154e244c1d194ed05a355
Jared May <myxjar002@myuct.ac.za>
2021-08-26 23:47:02 +0200

    Driver Class accepts folder as argument

--- Driver.cpp
+++ Driver.cpp
@@ -4,9 +4,9 @@ Jared May
 VFDS DRIVER CLASS
 */
 
-#include <fstream>
+//#include <fstream>
 #include <iostream>
-#include <sstream>
+//#include <sstream>
 #include <string>
 #include <vector>
 
@@ -16,6 +16,36 @@ VFDS DRIVER CLASS
 #include "Fracture.h"
 #include "CTReader.h"
 
+//find image dimension from folder name
+int findDim(std::string str)
+{
+    int dim = 0;
+    std::string tmp = "";
+    //for (int i = str.length()-3; i < str.length(); i++)
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (std::isdigit(str[i]))
+        {
+            tmp.push_back(str[i]);
+        }
+    }
+    dim = std::stoi(tmp);
+    return(dim);
+}
+
+//find shape name from folder name
+std::string findShape(std::string str)
+{
+    std::string tmp = "";
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (!std::isdigit(str[i]))
+        {
+            tmp.push_back(str[i]);
+        }   
+    }
+    return(tmp);
+}
 
 int main(int argc, char* argv[])
 {
@@ -23,10 +53,10 @@ int main(int argc, char* argv[])
 
     imgread::CTReader ctr;
     std::string files = "";
+    std::string shape = "";
     int dim = 0;
-    std::stringstream ss;
-    //get image folder name
     
+    //get image folder name as argument
     if (argc==1)
     {
         std::cout << "Please provide a folder name as an argument" << std::endl;
@@ -34,87 +64,19 @@ int main(int argc, char* argv[])
     }
     else
     {
-        //./drv cross128
         files = argv[1];
-        //std::cout << files[3] << std::endl;
-        /*ss << files;
-        std::string tmp;
-        int found;
-        
-        while (!ss.eof()) 
-        {
-        /// extracting word by word from stream
-        ss >> tmp;
-  
-        // Checking the given word is integer or not
-        if (std::stringstream(tmp) >> found)
-            std::cout << found << " ";
-  
-        // To save from space at the end of string
-        tmp = "";
-        }*/
-        //int index = files.find
+        shape = findShape(files);
+        dim = findDim(files);
+
         std::cout << "CTReader start" << std::endl;
-        imgdata::Voxel*** vox = ctr.imgread::CTReader::readPGMStack("cross", 128);
-        std::cout << "split merge start" << std::endl;
-        std::vector<imgdata::Fracture> frac = func::splitMerge(vox, 128, 128, 128);
+        imgdata::Voxel*** vox = ctr.imgread::CTReader::readPGMStack(shape, dim);
+        std::cout << "CTReader end" << std::endl;
+        std::cout << "Split/Merge start" << std::endl;
+        std::vector<imgdata::Fracture> frac = func::splitMerge(vox, dim, dim, dim);
+        std::cout << "Split/Merge end" << std::endl;
         //std::cout << "delete pgm stack" << std::endl;
         //ctr.imgread::CTReader::deletePGMStack(vox, 128);   
     }
     
     return 0;
-
-    //my old useless code
-    /* 
-    std::vector<std::string> pgm_lines;
-    std::string filename = "";
-    int w=0,h=0,r=0,c=0;
-
-    if (argc==1)
-    {
-        std::cout << "Please input a file name" << std::endl;
-        return 1;
-    }
-    else
-    {
-        filename = argv[1];
-        std::ifstream in(filename);
-        if(!in)
-        {
-            std::cout << "Couldn't open file " << filename << std::endl;
-            return 1;
-        }
-        std::stringstream ss;
-        std::string line = "";
-
-        //PGM:Version (Magic no)
-        getline(in,line);
-        if (line.compare("P5")!=0)
-        {
-            std::cerr << "Not PGM" << std::endl;
-        }
-        else
-        {
-           std::cout << "Version: " << line << std::endl;
-        }
-
-        //Comment
-        getline(in,line);
-        std::cout << "Comment: " << line << std::endl;
-        
-
-        ss << in.rdbuf();
-
-        ss >> c >> r;
-        while(std::getline(in, line))
-        {
-            pgm_lines.push_back(line);
-            std::cout << line << std::endl;
-        }
-        in.close();
-
-        std::cout << pgm_lines.size() << " lines in file" << std::endl;
-        
-    }*/
-
 }
