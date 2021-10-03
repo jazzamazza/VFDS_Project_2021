5d51688b4d58bdc9b115d280d7822b84cd0a3b0e
Iain-Hutch <iainhutch@iafrica.com>
2021-10-03 15:47:13 +0200

    Completed functionality of median filter in MedianFilter class

--- Filter.h
+++ Filter.h
@@ -4,6 +4,8 @@
 #ifndef FILTER_H
 #define FILTER_H
 #include "CTReader.h"
+#include "Voxel.h"
+#include "ImageProcessor.h"
 #include <vector>
 #include <cmath>
 #include <algorithm>
@@ -12,12 +14,15 @@ namespace imgfltr {
     template <typename T>
     class Filter {
         private:
+            // length of one side of the data cube
             int size;
         public:
             Filter() : size (0) {}
             Filter(int size) : size(size) {}
             ~Filter() {}
-            virtual T *** execute(T *** & source);
+            virtual T *** execute(const T *** & source, int n_size);
+            imgdata::Voxel *** toVoxel(const T *** & source);
+
     };
 
     template <typename T>
@@ -25,7 +30,9 @@ namespace imgfltr {
             public:
                 MedianFilter();
                 MedianFilter(int size);
-                T *** execute(T *** & source);
+
+                T *** execute(const T *** & source, int n_size);
+                std::vector<T> collectNeighbours(const T *** & source, int n_size, int depth, int col, int row);
                 T findMedian(const std::vector<T> & neighbours);
     };
 }
