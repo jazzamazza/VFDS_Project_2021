09f8f930389d272935a0ead866e8be923dfa3c15
Iain-Hutch <iainhutch@iafrica.com>
2021-10-05 15:32:51 +0200

    Added parameters for local mean and standard deviation in the AdaptiveThreshold class

--- src/AdaptiveThreshold.cpp
+++ src/AdaptiveThreshold.cpp
@@ -3,10 +3,9 @@
 // 04/10/21
 
 #include "AdaptiveThreshold.h"
-#include <iostream>
 
-denoise::AdaptiveThreshold::AdaptiveThreshold() : dim(0) {}
-denoise::AdaptiveThreshold::AdaptiveThreshold(int dim) : dim(dim) {}
+denoise::AdaptiveThreshold::AdaptiveThreshold() : dim(0), n_size(0), sigma_s(0), sigma_m(0) {}
+denoise::AdaptiveThreshold::AdaptiveThreshold(int dim, int n_size, double sigma_s, double sigma_m) : dim(dim), n_size(n_size), sigma_s(sigma_s), sigma_m(sigma_m) {}
 denoise::AdaptiveThreshold::~AdaptiveThreshold() {}
 
 // returns a histogram containing the frequency of each pixel intensity across the source image
@@ -32,6 +31,7 @@ void denoise::AdaptiveThreshold::getMean(int start, int end, double & mean, std:
     mean = mean/n;
 }
 
+// determine an adaptive threshold via successive refinement of the peak positions
 unsigned char denoise::AdaptiveThreshold::getThreshold(std::vector<int> & histogram, int max) {
     // Initialise the starting threshold T0 as the weighted average of the pixel intensities, based on their appearance in the hisogram
     double threshold = 0;
@@ -62,17 +62,58 @@ unsigned char denoise::AdaptiveThreshold::getThreshold(std::vector<int> & histog
 
 void denoise::AdaptiveThreshold::execute(unsigned char *** & source, unsigned char *** & target, int depth) {
     unsigned char *** src_copy = source;
-    // get the max pixel value in the image
+    // get the total cumulative value of the dataset and max pixel value in the image
     int max = 0;
+    double total = 0;
+
     for (int k = 0; k < dim; ++k) {
         for (int l = 0; l < dim; ++l) {
+            // accumulate the total
+            total += source[depth][k][l];
             if (source[depth][k][l] > max)
                 max = source[depth][k][l];
         }
     }
+    
 
+    //calculate population mean
+    double mean = total/(dim*dim);
+    double var = 0;
+
+    // calculate the global variance for the dataset and normailize the value
+    for (int y = 0; y < dim; ++y) {
+        for (int x = 0; x < dim; ++x) {
+            var += std::pow(((double)source[depth][y][x]-mean), 2);
+        }
+    }
+    var = var/std::pow(dim,2);
+
+    // if the variance across the entire dataset is below some acceptible value
+    // recognise as a noisy image with no part data
+    if(var < 100) {
+        for (int i = 0; i < dim; ++i) {
+            for (int j = 0; j < dim; ++j) {
+                // set all pixels to 0 rather than thresholding
+                target[depth][i][j] = 0;
+            }
+        }
+        return;
+    }
+
+    // new code using local histograms over neighbourhoods of pixels
+    // iterate over 2d image coordinates
+    for (int i = 0; i < dim; ++i) {
+        for (int j = 0; j < dim; ++j) {
+
+        }
+    }
+
+    // older code using a global histogram
+    /*
     std::vector<int> histogram = getHistogram(src_copy, depth, max);
     unsigned char threshold = getThreshold(histogram, max);
+    
+    // employ threshold across the dataset
     for (int i = 0; i < dim; ++i) {
         for (int j = 0; j < dim; ++j) {
             if (source[depth][i][j] < threshold) {
@@ -83,4 +124,5 @@ void denoise::AdaptiveThreshold::execute(unsigned char *** & source, unsigned ch
             }
         }
     }
+    */
 }   
--- src/AdaptiveThreshold.h
+++ src/AdaptiveThreshold.h
@@ -4,14 +4,19 @@
 #ifndef ADAPTIVETHRESHOLD_H
 #define APADTIVETHRESHOLD_H
 #include <vector>
+#include <cmath>
 
 namespace denoise {
     class AdaptiveThreshold{
         private:
-            int dim;
+            int dim, n_size;
+            // parameter to assign a weighting to the std deviation of a neighbourhood of pixels in the threshold calculation
+            double sigma_s;
+            // parameter to assign a weighting to the mean of a neighbourhood of pixels in the threshold calculation
+            double sigma_m;
         public:
             AdaptiveThreshold();
-            AdaptiveThreshold(int dim);
+            AdaptiveThreshold(int dim, int n_size, double sigma_s, double sigma_m);
             ~AdaptiveThreshold();
 
             void execute(unsigned char *** & source, unsigned char *** & target, int depth);
